daa42ccc2edec6a3315cde0bed20fe4b85204f28
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-10 21:57:01 +0530

    added authentication function

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const token = req.headers['authorization'];
+    console.log(req.headers,"headers")
 
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
@@ -9,13 +10,14 @@ const authenticateToken = (req, res, next) => {
     
     try {
         const tokenVerification = jwt.verify(token, 'secret-code');
-        console.log("token Verification", tokenVerification);
-        req.email = tokenVerification.email;
+        console.log("token from headers", tokenVerification);
+        req.body.email = tokenVerification.userEmail;
+        console.log(tokenVerification,"toe")
         next();
     }
 
     catch (error) {
-        return res.status(403).json({ message: "Invalid token" });
+        return res.status(403).json({ message: "Invalid token",error });
     }
 
 
