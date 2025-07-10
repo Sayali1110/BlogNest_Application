01c55fd4c2c06f49905b5fc5140082bd48b1368b
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-10 22:02:15 +0530

    added route for sign up

--- backend/routes/signUpRoute.js
+++ backend/routes/signUpRoute.js
@@ -1,21 +1,26 @@
-const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
-
-const { registerUser } = require('../services/userService')
-
+const { registerUser } = require('../services/userService');
 
 router.post('/', async (req, res) => {
+  const { username, email, password, bio, image } = req.body;
 
-    const { username, email, password } = req.body;
+  try {
+    console.log("in sigunp router");
 
-    try {
-        await registerUser(username, email, password);
-        res.status(201).json({ message: 'User Registered Successfully' });
-    }
-    catch (error) {
-        res.status(500).json({ error: 'Error registering user' });
-    }
+    const result = await registerUser(username, email, password, bio, image);
+    console.log("user from servie", result);
+
+    const Bio = result?.user?.bio;
+    const Image = result?.user?.image;
+
+    const User = { username: result.user.username, bio: Bio, image: Image, token: result.token, email: result.user.email };
+
+    res.status(201).json({ User });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: 'Error registering user' });
+  }
 });
 
 module.exports = router;
