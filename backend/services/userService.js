fbc70d7624125df320925c7fab51bf962d34d633
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-10 22:03:17 +0530

    added service for user registration

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,17 +1,25 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
-async function registerUser(username, email, password){
+async function registerUser(username, email, password, bio, image) {
+    console.log("para received", username, email, password, bio, image);
+
     const hasedPassword = await bcrypt.hash(password, 10);
-
+    console.log("Parameters received in registerUser:", username, email, password, bio, image);
+    
     const user = await User.create({
         username,
         email,
         password: hasedPassword,
+        bio,
+        image
     });
 
-    console.log("user", user)
-    return user
+    const token = jwt.sign({ userEmail: user.email }, 'secret-code', { expiresIn: '24h' });
+    console.log("token generation for sigup", token);
+
+    return { user, token };
 }
 
 module.exports = {
