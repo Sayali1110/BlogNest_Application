ade01066e51bd4c372e534436a2202921060b773
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-14 21:49:22 +0530

    added entry point for backend service

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.use(express.json()); 
+const sequelize = require('./db');
 
-require('./db');
-require('./models/association'); 
+require('./models/User');
+require('./models/Article');
+require('./models/Tag');
 
+require('./models/association');
 
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log("All models synced successfully");
+  })
+
+app.use(express.json());
 const authentication = require('./middleware/authentication');
 
 const homeRoute = require('./routes/homeRoute');
@@ -15,12 +23,11 @@ const loginRoute = require('./routes/loginRoute');
 const articleRoute = require('./routes/articleRoute');
 const tagRoute = require('./routes/tagRoute')
 
+app.use('/', homeRoute);
 app.use('/signUp', signUpRoute);
-app.use('/login',authentication, loginRoute);
-app.use('/',authentication, homeRoute); 
-
-app.use('/article', authentication,articleRoute);
-app.use('/tags', authentication, tagRoute )
+app.use('/login', authentication, loginRoute);
+app.use('/article', articleRoute);
+app.use('/tags', tagRoute)
 
 app.listen(6000, function () {
   console.log("server is running on port no. 6000");
