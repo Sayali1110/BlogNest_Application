592fba3485b3f0f03df9b903290a56bcd8a2afb4
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-15 21:10:30 +0530

    added bussiness logic for tags

---
+++ backend/services/tagService.js
@@ -0,0 +1,40 @@
+const { Model } = require('sequelize');
+const Tag = require('../models/Tag');
+const Article = require('../models/Article');
+const getAllTags = async (id) => {
+
+    if (!id) {
+        return await Tag.findAll({
+            attributes: ['name'],
+        });
+    }
+
+    return await Article.findAll({
+        where: { id: id },
+        attributes: ['tagList'],
+    })
+}
+
+
+
+const createTags = async (tags) => {
+    if (!Array.isArray(tags) || tags.length === 0) {
+        throw new Error('Tags are empty or not an array');
+    }
+
+    const createdTags = await Promise.all(
+        tags.map(async (tagName) => {
+            const [tag] = await Tag.findOrCreate({
+                where: { name: tagName }
+            });
+            return tag;
+        })
+    );
+
+    return createdTags;
+};
+
+module.exports = {
+    getAllTags,
+    createTags
+};
