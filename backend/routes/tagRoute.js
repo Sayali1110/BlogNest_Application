35bcf621aad590a70b8d05272f4aa54cf3b0c710
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-15 21:12:22 +0530

    added route handler for tags

--- backend/routes/tagRoute.js
+++ backend/routes/tagRoute.js
@@ -1,42 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const Tag = require('../models/Tag');
+const { getAllTags, createTags } = require('../services/tagService')
 
-router.post('/', async (req, res) => {
+router.get('/allTags', async (req, res) => {
+  try {
+    const ArticleId = req.query.id;
+    const tags = await getAllTags(ArticleId);
+    res.status(200).json(tags);
+  } catch (error) {
+    console.log("error fethcing tags", error);
+    res.status(500).json("Internal Server Error");
+  }
+
+});
+
+
+router.post('/tags', async (req, res) => {
   console.log("in tags route");
   try {
     const { tags } = req.body;
+    console.log("tags for checking", tags);
 
-    console.log("tags for checking", tags)
-
-    if (!Array.isArray(tags) || tags.length === 0) {
-      return res.status(400).json({ message: 'tags are empty' });
-    }
-
-    //accepts array
-    const createdTags = await Promise.all(
-      tags.map(async tagName => {
-        const [tag] = await Tag.findOrCreate({
-          where: { name: tagName }
-        });
-        return tag;
-      })
-    );
-
-
-    //single value
-    // const tag = await Tag.create({ 
-    //   name:tags
-    // })
-
-
+    const createdTags = await createTags(tags);
     res.status(201).json({ message: 'tags created', createdTags });
 
-  }
-  catch (error) {
+  } catch (error) {
     console.error("error creating tags", error);
-    res.status(500).json({ message: 'internal server error', error });
+    res.status(500).json({ message: 'internal server error', error: error.message });
   }
 });
+
 
 module.exports = router;
