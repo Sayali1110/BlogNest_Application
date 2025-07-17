4177ffcaeb3ad380ba910d20c4c5f06f1ea03c40
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-17 11:57:45 +0530

    added service for fetching article with pagination

--- backend/routes/articleRoute.js
+++ backend/routes/articleRoute.js
@@ -4,18 +4,39 @@ const Article = require('../models/Article');
 const Tag = require('../models/Tag');
 const User = require('../models/User');
 
-const { getAllArticles, createArticle } = require('../services/articleService')
+const { getAllArticles, createArticle, getAllMyArticles } = require('../services/articleService')
 
 router.get('/articles', async (req, res) => {
   try {
-    const tagName = req.query.tag;
-    const articles = await getAllArticles(tagName);
+    const tag = req.query.tag;
+    const limit = req.query.limit || 3;
+    const offset = req.query.offset || 0;
+
+
+    console.log("article limit", limit);
+    console.log("article offset", offset);
+
+    const articles = await getAllArticles(tag, limit, offset);
     res.status(200).json(articles);
   } catch (error) {
     console.error('Error fetching articles:', error);
     res.status(500).json({ message: 'Failed to fetch articles' });
   }
 });
+
+router.get('/feed', async (req, res) => {
+  try {
+    const author = req.query.author;
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    const articles = await getAllMyArticles(author, limit, offset);
+    res.status(200).json(articles);
+  }
+  catch (error) {
+    console.log("error fetching feed", error);
+    res.status(500).json("internal server error");
+  }
+})
 
 
 router.post('/article', async (req, res) => {
