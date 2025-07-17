898d332535efec3a7f09ffdb333e9bb48b815170
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-17 11:59:04 +0530

    added service for fetching articles using particular tag with pagination

--- backend/services/articleService.js
+++ backend/services/articleService.js
@@ -2,46 +2,72 @@ const Article = require('../models/Article');
 const User = require('../models/User');
 const Tag = require('../models/Tag');
 
-const getAllArticles = async (tagName) => {
+const getAllArticles = async (tagName, limit, offset) => {
+    console.log("limit and offset received", limit, offset);
 
+    //fetch article if tag is not present
     if (!tagName) {
+        console.log("limit and offset", limit, offset)
         return await Article.findAll({
+            limit: limit,
+            offset: limit * offset,
+            order: [['createdAt']],
             include: [
                 {
                     model: User,
                     as: 'author',
                     attributes: ['username', 'email', 'bio', 'image']
                 },
-                {
-                    model: Tag,
-                    as: 'tags',
-                    attributes: ['name'],
-                    through: { attributes: [] }
-                }
             ]
-        });
-    };
-    //find article Id with maatching tag
-    //fetch articles from that tag
-    const tag = await Tag.findOne({ where: { name: tagName } });
-    console.log("Article Id", tag)
-    if (!tag) {
-        return [];
+        })
     }
-    return await Article.findAll({
-        where: { id: tag.articleIdList },
-        include: [
-            {
-                model: User,
-                as: 'author',
-                attributes: ['username', 'email', 'bio', 'image']
+
+    //fetch articles if tag is present
+    else {
+            const tag = await Tag.findOne({ where: { name: tagName } });
+            console.log("Article Id", tag)
+            if (!tag) {
+                return [];
             }
-        ]
+            return await Article.findAll({
+                where: { id: tag.articleIdList },
+                limit: limit,
+                offset: limit * offset,
+                include: [
+                    {
+                        model: User,
+                        as: 'author',
+                        attributes: ['username', 'email', 'bio', 'image']
+                    },
+                ]
+            });
+    }
+}
+
+
+const getAllMyArticles = async (author, limit, offset) => {
+    //match username to author = User retun uid
+    //from uid find article id = Article return article 
+    const user = await User.findOne({ where: { username: author } });
+    console.log("username found", user);
+    const uId = user.id;
+    console.log("userId", uId);
+
+    return await Article.findAll({
+        where: { userId: uId },
+        limit: limit,
+        offset: limit * offset,
     });
+
 }
 
 
 
+
+
+
+
+//post article
 const createArticle = async (data) => {
     const { slug, title, description, body, favoritedCount, favorited, email, tags } = data;
 
@@ -97,5 +123,5 @@ const createArticle = async (data) => {
     return { message: "Article created successfully", article: transformedArticle };
 };
 
-module.exports = { getAllArticles, createArticle };
+module.exports = { getAllArticles, createArticle, getAllMyArticles };
 
