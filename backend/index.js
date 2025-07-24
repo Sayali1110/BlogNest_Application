7abeab11fea09e6030a70d1c8052150bd4695573
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:42:46 +0530

    main entry point for backend service

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
+const cors = require('cors');
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(express.json());
 const sequelize = require('./db');
 
 require('./models/User');
 require('./models/Article');
 require('./models/Tag');
+require('./models/Comment');
 
 require('./models/association');
 
-sequelize.sync({ alter: true })
+sequelize.sync()
   .then(() => {
     console.log("All models synced successfully");
   })
 
-app.use(express.json());
+
 const authentication = require('./middleware/authentication');
 
 const homeRoute = require('./routes/homeRoute');
@@ -22,14 +31,29 @@ const signUpRoute = require('./routes/signUpRoute');
 const loginRoute = require('./routes/loginRoute');
 const articleRoute = require('./routes/articleRoute');
 const tagRoute = require('./routes/tagRoute');
-
-app.use('/', homeRoute);
-app.use('/signUp', signUpRoute);
-app.use('/login', authentication, loginRoute);
-app.use('/', articleRoute);
-app.use('/', tagRoute)
+const myArticlesRoute = require('./routes/myArticlesRoute');
+const profileRoute = require('./routes/profileRoute');
+const feedArticles = require('./routes/feedRoute');
+const globalArticles = require('./routes/globalArticleRoute');
 
 
-app.listen(6000, function () {
-  console.log("server is running on port no. 6000");
+app.use('/api/home', homeRoute);
+app.use('/api/users', signUpRoute);
+app.use('/api/users/login', loginRoute);
+
+//app.use('/api/myArticles',authentication, myArticlesRoute);//myArticles, favoriteArticles
+app.use('/api/articles/feed', authentication, feedArticles);//user feed
+app.use('/api/articles', globalArticles );//get all articles, comments
+
+
+app.use('/api/article', authentication, articleRoute);// post 1 article, like article, post comment 
+app.use('/api/tags', tagRoute);
+
+app.use('/api/profiles',authentication, profileRoute);//follow service
+
+
+
+
+app.listen(5000, function () {
+  console.log("server is running on port no. 5000");
 })
