6a6b631e28b379a6799fd277a0ddcb9b19a032b6
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 18:00:46 +0530

    added backend main url

--- frontend/src/Components/constants.ts
+++ frontend/src/Components/constants.ts
@@ -1,1 +1,4 @@
-export const Main_URL = "https://conduit-realworld-example-app.fly.dev/api";
+// export const Main_URL = "https://conduit-realworld-example-app.fly.dev/api";
+
+export const Main_URL = "http://localhost:5000/api";
+
