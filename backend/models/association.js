7f56140b7ed618bf23a6c9ffa0693bfe8176e5e3
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:46:48 +0530

    added relationship between articles, tags, comments and User

--- backend/models/association.js
+++ backend/models/association.js
@@ -2,11 +2,14 @@ const sequelize = require('../db');
 const User = require('../models/User')
 const Article = require('../models/Article');
 const Tag = require('../models/Tag');
+const Comment = require('../models/Comment');
 
-
+//one to one
 Article.belongsTo(User, { foreignKey: 'userId', as: 'author' });
+//one to many
 User.hasMany(Article, { foreignKey: 'userId', as: 'articles' });
 
+//Many to Many 
 Article.belongsToMany(Tag, {
   through: 'ArticleTags',
   as: 'tags',
@@ -18,13 +21,12 @@ Tag.belongsToMany(Article, {
   foreignKey: 'tagId',
 });
 
-
-
-
-
-
-// Article.hasMany(Tag, { foreignKey: 'articleId', as: 'tags' });
-// Tag.belongsTo(Article, { foreignKey: 'articleId', as: 'article' });
+//one article has many comments
+Article.hasMany(Comment, {foreignKey: 'articleId', as: 'Comments'});
+//one comment has one user
+Comment.belongsTo(Article, {foreignKey:'articleId', as:'articles'});
+//one comment celongs to one user
+Comment.belongsTo(User,{foreignKey:'userId', as: 'user'} );
 
 module.exports = { User, Article, Tag };
 
