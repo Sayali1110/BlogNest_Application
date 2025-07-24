ce1df7fc46a0c40946a1af7c1639053acdf41dac
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:59:00 +0530

    added bussiness logic for following particular author

--- frontend/src/Components/Services/postFollow.ts
+++ frontend/src/Components/Services/postFollow.ts
@@ -1,11 +1,12 @@
 import axios from "axios"
 import { Main_URL } from "../constants"
 
-export const postFollow = async (slug: string, username: string, isFollow: boolean, favoritesCount:number) => {
+export const postFollow = async (slug: string, username: string, isFollow: boolean, favoritesCount: number) => {
     try {
+        console.log("following data", slug, isFollow);
         const token = localStorage.getItem("token");
         const response = await axios({
-            method: isFollow ? "DELETE" : "POST",
+            method: "POST",
             url: `${Main_URL}/profiles/${username}/follow`,
             data: {
                 article: {
@@ -20,7 +21,7 @@ export const postFollow = async (slug: string, username: string, isFollow: boole
                 "Content-Type": "application/json",
             },
         });
-
+        console.log("response", response);
         return response.data.profile;
     }
     catch (error) {
