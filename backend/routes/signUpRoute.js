6e8b568f861989e473135956d26eee4dcf8208bc
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:49:33 +0530

    added routes for registration functionality

--- backend/routes/signUpRoute.js
+++ backend/routes/signUpRoute.js
@@ -5,20 +5,22 @@ const { registerUser } = require('../services/userService');
 router.post('/', async (req, res) => {
   console.log("in signup route");
 
-  const { username, email, password, bio, image } = req.body;
+  const { username, email, password, bio, image } = req.body.user;
 
   try {
-    console.log("in sigunp router");
+
+    console.log("request body from signup route", req.body.user);
 
     const result = await registerUser(username, email, password, bio, image);
     console.log("user from servie", result);
 
+
     const Bio = result?.user?.bio;
     const Image = result?.user?.image;
 
-    const User = { username: result.user.username, bio: Bio, image: Image, token: result.token, email: result.user.email };
+    const user = { username: result.user.username, bio: Bio, image: Image, token: result.token, email: result.user.email };
 
-    res.status(201).json({ User });
+    res.status(201).json({ user });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ error: 'Error registering user' });
