b9f18111139fff87cd4deec0f501a697815d3a5d
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:52:58 +0530

    added business logic for registration functionality with token generation

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -3,7 +3,9 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 async function registerUser(username, email, password, bio, image) {
-    console.log("para received", username, email, password, bio, image);
+    console.log("username", username);
+    console.log("para received in user service", username, email, password, bio, image);
+
 
     const hasedPassword = await bcrypt.hash(password, 10);
     console.log("Parameters received in registerUser:", username, email, password, bio, image);
@@ -16,7 +18,7 @@ async function registerUser(username, email, password, bio, image) {
         image
     });
 
-    const token = jwt.sign({ userEmail: user.email }, 'secret-code', { expiresIn: '24h' });
+    const token = jwt.sign({ userEmail: user.email }, 'aligned-automation', { expiresIn: '30d' });
     console.log("token generation for sigup", token);
 
     return { user, token };
