d791512bacf8d279ae95fb79ba119d9665838c92
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-07-24 17:50:04 +0530

    added routes for fethcing all tags

--- backend/routes/tagRoute.js
+++ backend/routes/tagRoute.js
@@ -3,16 +3,24 @@ const router = express.Router();
 const Tag = require('../models/Tag');
 const { getAllTags, createTags } = require('../services/tagService')
 
-router.get('/allTags', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const ArticleId = req.query.id;
     const tags = await getAllTags(ArticleId);
-    res.status(200).json(tags);
+
+   
+if (!ArticleId) {
+      tagList = tags.map(tag => tag.name);
+    } else {
+      tagList = tags.length > 0 ? tags[0].tagList : [];
+    }
+
+    res.status(200).json({ tags: tagList });
+
   } catch (error) {
     console.log("error fethcing tags", error);
     res.status(500).json("Internal Server Error");
   }
-
 });
 
 
@@ -30,6 +38,5 @@ router.post('/tags', async (req, res) => {
     res.status(500).json({ message: 'internal server error', error: error.message });
   }
 });
-
-
+  
 module.exports = router;
