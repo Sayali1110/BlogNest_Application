5be3c9f44b30e219038082ec1b5ed2e65033907c
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-08-25 15:50:30 +0530

    added file for fethcing user data from API

--- frontend/src/Components/Services/getUser.ts
+++ frontend/src/Components/Services/getUser.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
+import { Main_URL } from "../constants";
 
 export const getUser = async () => {
+  console.log("in get user function");
   const token = localStorage.getItem("token");
+
   if (!token) return null;
 
-  const response = await axios.get("https://conduit-realworld-example-app.fly.dev/api/user", {
+  const response = await axios.get(`${Main_URL}/user`, {
     headers: {
       Authorization: `Token ${token}`
     }
   });
 
-  return response.data.user;
+  return response.data.user || response.data;
 };
