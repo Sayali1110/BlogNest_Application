fa03f92bd9f547e79e1c7165c50d59a5521d8e15
Sayali Sonawane <sayali.sonawane@alignedautomation.com>
2025-08-25 15:52:15 +0530

    added route for fethcing download information, most liked articles, no of articles per article

--- backend/routes/dashBoardRoute.js
+++ backend/routes/dashBoardRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {fetchDownloadData, fetchData, downloadActivity } = require('../services/dashBoardService');
+const { fetchDownloadData, fetchData, downloadActivity, totalLikes } = require('../services/dashBoardService');
 
 router.get('/pieChart', async (req, res) => {
     try {
@@ -12,7 +12,6 @@ router.get('/pieChart', async (req, res) => {
         console.error("error fetching total articles data");
         res.status(500).json({ message: "Internal server error", error });
     }
-
 });
 
 
@@ -28,14 +27,26 @@ router.get('/barChart', async (req, res) => {
 });
 
 router.get('/downloadActivity', async (req, res) => {
-    try{
+    try {
         const data = await downloadActivity();
         console.log("all download history", data);
         res.status(200).json(data);
-    }catch(error){
-         console.error("error fetching downloaded articles activity");
+    } catch (error) {
+        console.error("error fetching downloaded articles activity");
         res.status(500).json({ message: "Internal server error", error });
 
     }
+})
+
+router.get('/mostLikedArticles', async (req, res) => {
+    try {
+        const data = await totalLikes();
+        console.log("all download history", data);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("error fethcing most liked articles");
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+
 })
 module.exports = router;
